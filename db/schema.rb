c2875eb9b2bee1594fd82769f10c830fe6a08709
zofier <zof11@excite.co.jp>
2016-01-29 16:59:09 +0900

    fullname

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129071115) do
+ActiveRecord::Schema.define(version: 20160129075819) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 20160129071115) do
     t.integer  "height"
     t.string   "marital",                limit: 11
     t.string   "TEL",                    limit: 11
+    t.string   "fullname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
