93efb141cb30e5951706903b62166b4561f4473c
zofier <zof11@excite.co.jp>
2016-02-03 09:41:13 +0900

    不要なcontroller削除

--- app/helpers/article2_helper.rb
+++
@@ -1,2 +0,0 @@
-module Article2Helper
-end
--- test/controllers/article3_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Article3ControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
