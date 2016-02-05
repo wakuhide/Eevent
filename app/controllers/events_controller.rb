3ca502e35fdfa986ee6d412a860cf4ff663b7279
zofier <zof11@excite.co.jp>
2016-02-05 17:40:03 +0900

    routes整理、eventsコントローラーから不要なもの排除し、viewも消した

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,18 +5,8 @@ class EventsController < ApplicationController
     @events = Event.order("date DESC").page(params[:page]).per(6)
   end
 
-  def show
-    @event = Event.find(params[:id])
-  end
-
   def detail
     @event = Event.find(params[:event_id])
-  end
-
-  def search
-  end
-
-  def create
   end
 
   def send_mail
@@ -26,6 +16,4 @@ class EventsController < ApplicationController
     SendMailToUsers.thanks_email('current_user.email').deliver
     redirect_to root_path
   end
-
-
 end
